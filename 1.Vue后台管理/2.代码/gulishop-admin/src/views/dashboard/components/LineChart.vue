e513c5dfc239aa21768513caf9e1d619ef84be76
HotJuly <949588380@qq.com>
2021-07-12 16:22:34 +0800

    75.v-charts介绍

--- 1.Vue后台管理/2.代码/gulishop-admin/src/views/dashboard/components/LineChart.vue
+++ 1.Vue后台管理/2.代码/gulishop-admin/src/views/dashboard/components/LineChart.vue
@@ -4,100 +4,101 @@
 
 <script>
 import echarts from "echarts";
-require("echarts/theme/macarons"); // echarts theme
-import resize from './mixins/resize';
-
+require("echarts/theme/macarons");
 export default {
   name: "LineChart",
-  mixins:[resize],
-  props: {
-    width: {
-      type: String,
-      default: "100%",
+  data() {
+    return {
+      chart: null,
+    };
+  },
+  props:{
+    width:{
+      type:String,
+      default:"100%"
     },
-    height: {
-      type: String,
-      default: "350px",
-    },
-    className: {
-      type: String
+    height:{
+      type:String,
+      default:"350px"
     },
     chartData:{
       type:Object,
-      require:true
+      required:true
     },
     yTitle:{
       type:String,
-      require:true
+      required:true
     }
   },
   mounted() {
-    this.chart = echarts.init(this.$el);
-    this.setOption(this.chartData);
+    // 初始化只需要做一次,后续只需要使用之前的实例对象即可
+    this.chart = echarts.init(this.$el,'macarons');
+    this.setOption();
   },
-  methods: {
-    setOption({expectedData,actualData}) {
-      const option = {
+  methods:{
+    setOption({expectedData,actualData}){
+      var option = {
+        title: {
+        },
+        // 控制数据显示
         tooltip: {
-          axisPointer: {
-            type: "cross",
-          },
-          trigger: "axis",
+           axisPointer:{
+             type:"cross"
+           },
+           trigger:"axis"
         },
-        legend:{
-          data:[
-            {
-              name:"预期"
-            },
-            {
-              name:"实际"
-            }
-          ]
+        // 控制内容显示开关
+        legend: {
+            data:['预期','实际']
         },
+        grid:{
+          left:40,
+          right:40,
+          top:40,
+          bottom:40
+        },
+        // x轴配置
         xAxis: {
-          type: "category",
-          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-          boundaryGap: false,
+            data: ["周一","周二","周三","周四","周五","周六","周日"],
+            boundaryGap:false
         },
+        // y轴配置
         yAxis: {
-          name: this.yTitle,
+          name:this.yTitle
         },
-        grid: {
-          left: 40,
-          right: 40,
-          top: 40,
-          bottom: 40,
-        },
-        series: [
-          {
-            name:"预期",
+        // 
+        series: [{
+            name: '预期',
+            type: 'line',
+            smooth:true,
             data: expectedData?expectedData:[0,0,0,0,0,0,0],
-            smooth: true,
-            type: "line",
-            itemStyle: {
-              color: "hotpink",
+            lineStyle:{
+              color:"hotpink"
             },
-            areaStyle: {
-              color: "#ccc",
-            },
-          },
-          {
-            name:"实际",
+            areaStyle:{
+              color:"#ccc"
+            }
+        },
+        {
+            name: '实际',
+            type: 'line',
+            smooth:true,
             data: actualData?actualData:[0,0,0,0,0,0,0],
-            smooth: true,
-            type: "line",
-            itemStyle: {
-              color: "skyblue",
-            },
-          },
-        ],
+            lineStyle:{
+              color:"skyblue"
+            }
+        }]
       };
       this.chart.setOption(option);
-    },
+    }
   },
   watch:{
-    chartData(newVal,oldVal){
-      this.setOption(newVal);
+    chartData:{
+      deep:true,
+      handler(newVal){
+        // console.log(newVal)
+        this.setOption(newVal)
+      }
     }
   }
 };
