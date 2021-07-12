516213c7fb2888aebf7742933ebfc720a9aaaec8
HotJuly <949588380@qq.com>
2021-07-12 11:55:05 +0800

    71.scoped作用域以及深度选择器的原理

--- 1.Vue后台管理/2.代码/gulishop-admin/src/views/product/sku/list.vue
+++ 1.Vue后台管理/2.代码/gulishop-admin/src/views/product/sku/list.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-      sku
+  <div id="sku">
+      <h1>喜欢我的请亮灯:</h1>
+      <h2>刘亦菲</h2>
+      <Child/>
   </div>
 </template>
 
 <script>
+import Child from './test/Child.vue';
 export default {
-    name:"SKU"
+    name:"SKU",
+    components:{
+      Child
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#sku{
+  ::v-deep h2{
+  color:red;
+  font-size:30px;
+}
+}
 
 </style>
