42732a2eec6f4bc7d19da336f39ce526f2e7084e
HotJuly <949588380@qq.com>
2021-08-06 12:01:30 +0800

    38.Vue-API(剩余内容)

--- 4.复习/02.代码/api_test/src/App.vue
+++ 4.复习/02.代码/api_test/src/App.vue
@@ -1,25 +1,52 @@
 <template>
   <div id="app" class="A">
-    <h1>{{msg}}</h1>
-    <h2>{{form.name}}</h2>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 ref="msg">{{msg}}</h1>
+    <h2 v-once>{{form.name}}</h2>
+    <!-- <h2>{{form.name | myFilter}}</h2> -->
+    <!-- <input type="text" v-model="form.name"> -->
+    <!-- <input type="text" :value="form.name" @input="(event)=>{form.name = event.target.value}"> -->
+
+    <!-- <HelloWorld ref="helloworld" class="A" :value.sync="form.name" msg="Welcome to Your Vue.js App"/> -->
+    <!-- <HelloWorld :value="form.name" @update:value="(value)=>{form.name = value}" msg="Welcome to Your Vue.js App"/> -->
+    <!-- <HelloWorld ref="helloworld" class="A" v-model="form.name" msg="Welcome to Your Vue.js App"/> -->
+    <!-- <HelloWorld :value="form.name" @input="(value)=>{form.name = value}" msg="Welcome to Your Vue.js App"/> -->
+
+    <HelloWorld ref="helloworld" class="A" :value.sync="form.name" msg="Welcome to Your Vue.js App">
+      <template v-slot:default>
+        <h1>大佐牛逼!!!</h1>
+      </template>
+       <template v-slot:huahua>
+        <h1>华华牛逼!!!</h1>
+      </template>
+       <template v-slot:scope="{row}">
+        <h1>{{row}}牛逼!!!</h1>
+      </template>
+    </HelloWorld>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+var mixin = {
+  created: function () { console.log(1) }
+}
+
 export default {
-  name: 'App',
   components: {
     HelloWorld
+  },
+  mixins:[mixin],
+  provide:{
+    sendMsg:"来自老父亲关爱"
   },
   data(){
     return {
       msg:"初始化数据",
       form:{
         name:123
-      }
+      },
+      test:"test111"
     }
   },
   a:3,
@@ -57,7 +84,16 @@ export default {
     //   console.log('Promise2')
     // })
 
-    console.log(this.$data,JSON.parse(JSON.stringify(this.$data)))
+    // console.log(this.$data,JSON.parse(JSON.stringify(this.$data)))
+    // console.log(this.$el)
+    // console.log('children',this.$children[0])
+    // this.$children[0]=null;
+    console.log('$refs',this.$refs.msg,this.$refs.helloworld)
+  },
+  filters:{
+    myFilter(value){
+      return value + "hello"
+    }
   }
 }
 </script>
--- 4.复习/02.代码/api_test/src/components/HelloWorld.vue
+++ 4.复习/02.代码/api_test/src/components/HelloWorld.vue
@@ -1,16 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 v-pre>{{ msg }}</h1>
+    <h3>{{sendMsg}}</h3>
+    <input type="text" :value="value" @input="changeValue">
+    <!-- 需要进行A和B组件之间的切换 -->
+    <!-- <A v-if="flag"></A>
+    <B v-else></B> -->
+    <!-- <component :is="com"></component> -->
+    <slot name="default"></slot>
+    <slot name="huahua"></slot>
+    <slot name="scope" :row="sendMsg"></slot>
+    <button @click="deleteCom">删除HelloWorld</button>
   </div>
 </template>
 
 <script>
+import A from './A.vue'
+import B from './B.vue'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      com:A
+    }
   },
-  a:5
+  props: {
+    msg: String,
+    value:String|Number,
+    formName:String|Number,
+  },
+  inject:["sendMsg"],
+  a:5,
+  model:{
+    event:"input123",
+    prop:"formName"
+  },
+  methods:{
+    changeValue(event){
+      // console.log('event',event)
+      var value = event.target.value;
+      // this.value = value;
+      // this.$emit('input123',value)
+      this.$emit('update:value',value)
+    },
+    deleteCom(){
+      console.log('deleteCom')
+      // destory的用处仅仅是将当前组件所有响应式全部断开,也就是说你后续对该组件进行任何修改,都不会影响到页面展示效果
+      // this.$destroy();
+      this.com=B;
+    }
+  },
+  mounted(){
+    console.log('test',this.$parent.test)
+    console.log('root',this.$root === this.$parent)
+  }
 }
 </script>
 
